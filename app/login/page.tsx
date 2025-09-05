e01c3e0d49dc147358e7d6d3e9ef83e80aa51f1c
TheRaj71 <theraj714@gmail.com>
2025-09-05 16:42:10 +0530

    login page ui addon

---
+++ app/login/page.tsx
@@ -0,0 +1,38 @@
+import { GalleryVerticalEnd } from "lucide-react"
+import Image from "next/image"
+
+import { LoginForm } from "@/components/login-form"
+
+export default function LoginPage() {
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="#" className="flex items-center gap-2 font-medium">
+            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+              <GalleryVerticalEnd className="size-4" />
+            </div>
+            <span style={{ fontSize: "120%" }}>जनसेतु.</span>
+          </a>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+      <div className="bg-muted relative hidden lg:block">
+        <div className="absolute inset-0 w-full h-full">
+          <Image
+            src="/ladyppl.png"
+            alt="Image"
+            fill
+            className="object-cover dark:brightness-[0.2] dark:grayscale"
+            style={{ objectPosition: "center 15%" }}
+            priority
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
